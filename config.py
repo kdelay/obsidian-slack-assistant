40130c6e59c6f83c104c247b18857170555a6efe
kdelay <kdelay20@gmail.com>
2026-03-24 11:29:00 +0900

    Remove hardcoded personal Obsidian path from config

--- config.py
+++ config.py
@@ -9,5 +9,5 @@ SLACK_BOT_TOKEN: str = os.environ["SLACK_BOT_TOKEN"]
 SLACK_APP_TOKEN: str = os.environ["SLACK_APP_TOKEN"]
 SLACK_CHANNEL_ID: str = os.environ.get("SLACK_CHANNEL_ID", "")
 ANTHROPIC_API_KEY: str = os.environ["ANTHROPIC_API_KEY"]
-OBSIDIAN_VAULT: str = os.environ.get("OBSIDIAN_VAULT", "/Users/lilly/Documents/Obsidian")
+OBSIDIAN_VAULT: str = os.environ["OBSIDIAN_VAULT"]
 BRIEFING_HOUR: int = int(os.environ.get("BRIEFING_HOUR", "7"))
